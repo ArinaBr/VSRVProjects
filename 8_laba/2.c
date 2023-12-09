6c996120a764a7ac27af1f33f66984768503b7cf
Arina <102375971+ArinaBr@users.noreply.github.com>
2023-12-10 00:21:06 +0300

    8 laba added

---
+++ 8_laba/2.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include <fcntl.h>
+
+int main(int argc, char *argv[]) {
+        char buf;
+        int fd_IN, fd_OUT;
+
+        if(argc == 2) {
+                fd_IN = open(argv[1], O_RDONLY, 0644);
+                close(0);
+                dup(fd_IN);
+        }
+
+        while(read(STDIN_FILENO, &buf, 1) > 0) {
+                write(STDOUT_FILENO, &buf, 1);
+        }
+
+}       
